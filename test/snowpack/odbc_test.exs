6e6c119be2d5dcdf697a36c7c32f2e17b8efd60e
Forest Carlisle <7829+forest@users.noreply.github.com>
2022-07-21 12:03:14 -0700

    fix: handle error response from LAST_QUERY_ID sql_query

--- test/snowpack/odbc_test.exs
+++ test/snowpack/odbc_test.exs
@@ -24,6 +24,18 @@ defmodule Snowpack.ODBCTest do
 
     {:ok, pid} = start_supervised({Snowpack, key_pair_opts()})
 
-    assert {:error, %Snowpack.Error{message: "bad things!"}} = Snowpack.query(pid, "select 1")
+    assert {:error, %Snowpack.Error{message: "bad things!"}} = Snowpack.query(pid, "select 1", [], parse_results: false)
+  end
+
+  @tag :capture_log
+  test "handle_call/2 query when last_query_id returns error" do
+    Mimic.set_mimic_global()
+
+    expect(:odbc, :sql_query, fn _, 'begin transaction;' -> :ok end)
+    expect(:odbc, :sql_query, fn _, 'SELECT LAST_QUERY_ID() as query_id;' -> {:error, :very_bad_things} end)
+
+    {:ok, pid} = start_supervised({Snowpack, key_pair_opts()})
+
+    assert {:error, %Snowpack.Error{message: "very_bad_things"}} = Snowpack.query(pid, "select 1")
   end
 end
